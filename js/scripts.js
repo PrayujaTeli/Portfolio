436753b657c4e5d6fc1b224c8a85dda8180d0426
Prayuja Teli <prayujateli@gmail.com>
2024-03-12 15:59:58 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
